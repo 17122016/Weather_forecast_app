f449f913b9d3189194b65e8b99171b5e6fd7c304
Nidhi Nayak <60571612+17122016@users.noreply.github.com>
2024-05-27 14:25:32 +0530

    Update InfoBox.jsx

--- weather_forecast_app/src/InfoBox.jsx
+++ weather_forecast_app/src/InfoBox.jsx
@@ -8,11 +8,8 @@ import SevereColdIcon from '@mui/icons-material/SevereCold';
 import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 import "./InfoBox.css"
 
-
-
-
 export default function InfoBox({info}){
-
+  
   let hot_url = "https://media.istockphoto.com/id/824845572/photo/thermometer-sun-high-degres-hot-summer-day-high-summer-temperatures.webp?b=1&s=170667a&w=0&k=20&c=8ijy1ixq0ALEpJIt9D4ElKJzayexgzXTDG2ZRCYVfHQ=";
   let cold_url = "https://plus.unsplash.com/premium_photo-1670493556860-13e006e6faa4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8d2ludGVyfGVufDB8fDB8fHww";
   let rain_url = "https://images.unsplash.com/photo-1503435824048-a799a3a84bf7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFpbnxlbnwwfHwwfHx8MA%3D%3D";
@@ -40,18 +37,11 @@ export default function InfoBox({info}){
           <p>TempMaximum = {info.tempMax}&deg;C</p>
           <p>TempMinimum = {info.tempMin}&deg;C</p>
           <p>Humidity    = {info.humidity}</p>
-          <p>Windspeed   = {info.windspeed}</p>
-        
+          <p>Windspeed   = {info.windspeed}</p>      
         </Typography>
       </CardContent>
-    
     </Card>
-
-  
     </div>
-
-    </div>
-
-    
+    </div>   
    );
 }
