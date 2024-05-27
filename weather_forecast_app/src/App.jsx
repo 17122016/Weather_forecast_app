11de313f31afce3486e4dd8df05085906d36bca9
Nidhi Nayak <60571612+17122016@users.noreply.github.com>
2024-05-27 14:24:02 +0530

    Update App.jsx

--- weather_forecast_app/src/App.jsx
+++ weather_forecast_app/src/App.jsx
@@ -4,22 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
-
-
 import Weather from './Weather';
+
+
 function App() {
- 
-
   return (
-    <>
-   
-   <Weather/>
-
-
-     
-     
+    <> 
+   <Weather/>          
     </>
   )
 }
-
 export default App
